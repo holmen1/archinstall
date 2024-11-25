f9fc7f49ec578b25017148414970a81b114f9916
holmen1 <holmen1@gmail.com>
2024-11-25 19:41:45 +0100

    relative line numbering

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,4 +4,8 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- Enable relative line numbering
+vim.wo.number = true
+vim.wo.relativenumber = true
+
 require("config.lazy")
