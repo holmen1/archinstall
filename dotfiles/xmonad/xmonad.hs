5dd6a680d5787761c1456c8ad14d00243e75e8ad
holmen1 <holmen1@gmail.com>
2025-04-16 19:34:04 +0200

    add xmobar

--- dotfiles/xmonad/xmonad.hs
+++ dotfiles/xmonad/xmonad.hs
@@ -3,9 +3,12 @@ import XMonad.Util.EZConfig
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Magnifier
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
 
 main :: IO ()
-main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+main = xmonad . ewmhFullscreen . ewmh . xmobarProp $ myConfig
 
 myTerminal      = "kitty"
 myModMask       = mod4Mask -- Rebind Mod to the Super key
